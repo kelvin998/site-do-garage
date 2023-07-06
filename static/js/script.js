a7969fa069398d5df922eeebc35c5eb47dc54c9b
kelvin998 <73448458+kelvin998@users.noreply.github.com>
2023-07-05 21:00:42 -0300

    primeiro commit

---
+++ static/js/script.js
@@ -0,0 +1,10 @@
+function showOverlay2(overlayId) {
+    var overlay = document.getElementById(overlayId);
+    overlay.style.display = "block";
+  }
+  
+  function hideOverlay(overlayId) {
+    var overlay = document.getElementById(overlayId);
+    overlay.style.display = "none";
+  }
+  
